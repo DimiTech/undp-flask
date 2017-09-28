d63db4e71ad5bf72c2eb35a99e2a0e65f859b610
Dusan Dimitric <dusan_dimitric@yahoo.com>
2017-09-28 19:59:26 +0200

    Switch from AJAX/JSON to multipart/form-data

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import uuid
 
 from flask         import Flask
 from flask         import request
-from flask         import jsonify
+from flask         import redirect
 from flask_migrate import Migrate
 
 app = Flask(__name__)
@@ -12,34 +12,18 @@ from db import Application
 
 migrate = Migrate(app, db)
 
-@app.route('/', methods=['GET'])
-def index():
-    return 'APPLICATION WORKS!'
-
-@app.route('/', methods=['POST'])
+@app.route('/challenges/open_data', methods=['POST'])
 def create_new_application():
     new_uuid = str(uuid.uuid4())
-    data = request.get_json()
-    new_application = Application(
-        uuid                       = new_uuid,
-        application_type_id        = data['application_type_id'],
-        title                      = data['title'],
-        relevant_interests         = data['relevant_interests'],
-        self_government            = data['self_government'],
-        contact_person             = data['contact_person'],
-        applicant_website          = data['applicant_website'],
-        project_duration_in_months = data['project_duration_in_months'],
-        other_applicants           = data['other_applicants'],
-        project_summaries          = data['project_summaries'],
-        detailed_description       = data['detailed_description'],
-        innovation                 = data['innovation'],
-        project_activities         = data['project_activities'],
-        expected_outcomes          = data['expected_outcomes'],
-        gender_approach            = data['gender_approach'],
-        project_budget             = data['project_budget']
+
+    print(request.form['title'])
+    print(request.form['project_duration_in_months'])
+    print(request.files['file_1'])
+    print(request.files['file_2'])
+
+    redirect_uri = 'http://%s/success.html?id=%s' % (
+        request.remote_addr,
+        new_uuid
     )
-    db.session.add(new_application)
-    db.session.commit()
-    
-    return jsonify({'uuid': new_uuid}) 
+    return redirect(redirect_uri, code=302)
 
