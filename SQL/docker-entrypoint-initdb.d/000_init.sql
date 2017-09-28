90b588a481ffa221d2107d014741c904f4a3b217
Dusan Dimitric <dusan_dimitric@yahoo.com>
2017-09-28 10:54:56 +0200

    Abandon SQL from here

--- SQL/docker-entrypoint-initdb.d/000_init.sql
+++ SQL/docker-entrypoint-initdb.d/000_init.sql
@@ -4,8 +4,112 @@ CREATE DATABASE UNDP;
 
 USE UNDP;
 
-CREATE TABLE test (
-    id CHAR(36) UNIQUE PRIMARY KEY
+-- -------------------------- OPEN DATA CHALLENGE -------------------------- -- 
+
+-- CREATE TABLE contact_people (
+-- 
+--     person_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+-- 
+--     -- Contact person.
+--     full_name   VARCHAR(255) NOT NULL,
+--     address     VARCHAR(255) NOT NULL,
+--     postal_code VARCHAR(100) NOT NULL,
+--     e_mail      VARCHAR(100) NOT NULL,
+--     phone       VARCHAR(100) NOT NULL,
+-- 
+--     PRIMARY KEY (person_id)
+-- 
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE application_types (
+
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (id)
+    
 );
 
-INSERT INTO test (id) VALUES ('JJAQUVB04JV8E92JC72IFO394UVJDAPV0119');
+INSERT INTO application_types (id, name) VALUES 
+    (1, 'open_data_challenge'),
+    (2, 'innovation_challenge');
+
+CREATE TABLE applications (
+
+    id                  INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    uuid                CHAR(36)     NOT NULL UNIQUE,
+    application_type_id INT UNSIGNED NOT NULL,
+    
+    -- Application details.
+    title                      VARCHAR(255)      NOT NULL,
+    relevant_interests         TEXT              NOT NULL,
+    self_government            VARCHAR(500)      NOT NULL,
+    contact_person             JSON              NOT NULL,
+    applicant_website          VARCHAR(2083)     NOT NULL,
+    project_duration_in_months SMALLINT UNSIGNED NOT NULL,
+
+    -- Other project applicants.
+    other_applicants JSON, -- [] of objects. TODO: Extract this into a table.
+
+    -- Project summaries.
+    project_summaries JSON NOT NULL, -- [] of objects. TODO: Extract this into a table.
+
+    -- Description of the project.
+    detailed_description TEXT NOT NULL,
+    innovation           TEXT NOT NULL,
+    project_activities   TEXT NOT NULL,
+    expected_outcomes    TEXT NOT NULL,
+    gender_approach      TEXT NOT NULL,
+    project_budget       TEXT NOT NULL, -- This should be probably split into NUMERIC and TEXT fields.
+    
+    PRIMARY KEY (id),
+    CONSTRAINT `fk_application_application_type` 
+        FOREIGN KEY (application_type_id) 
+        REFERENCES application_types (id)
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- CREATE TABLE applicants (
+-- 
+--     applicant_id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     application_id INT UNSIGNED NOT NULL,
+-- 
+--     -- Other project applicants (if applicable).
+--     name                 VARCHAR(500)  NOT NULL,
+--     contact_person_id    INT UNSIGNED  NOT NULL,
+--     organization_webiste VARCHAR(2083) NOT NULL,
+--     role_and_value       TEXT          NOT NULL,
+--     previous_experience  TEXT          NOT NULL,
+-- 
+--     PRIMARY KEY (applicant_id),
+--     CONSTRAINT `fk_applicant_application` 
+--         FOREIGN KEY (application_id) 
+--         REFERENCES applications (id),
+--     CONSTRAINT `fk_applicant_contact_person` 
+--         FOREIGN KEY (contact_person_id) 
+--         REFERENCES contact_people (person_id)
+-- 
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 
+-- 
+-- CREATE TABLE project_summaries ( 
+-- 
+--     summary_id     INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     application_id INT UNSIGNED NOT NULL,
+-- 
+--     -- Summary of the proposed project idea.
+--     language CHAR(2) NOT NULL, -- SR or EN, TODO: Give this field the ENUM type.
+--     summary  TEXT    NOT NULL,
+-- 
+--     PRIMARY KEY (summary_id),
+--     CONSTRAINT `fk_summary_application` 
+--         FOREIGN KEY (application_id) 
+--         REFERENCES applications (id)
+-- 
+-- )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- ------------------------------------------------------------------------- -- 
+
--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
+import os
+import uuid
+
 from flask import Flask
+from flask import request
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,27 +11,222 @@ app = Flask(__name__)
 # TODO: Create a sensible project structure.
 # TODO: Extract the DB related stuff to it's own module.
 
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+
+DB_CONN_STR = 'mysql+pymysql://test:test@%s:3306/UNDP' % MYSQL_HOST
+
+app.config['SQLALCHEMY_DATABASE_URI']        = DB_CONN_STR
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://test:test@db:3306/UNDP'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']  = True
 
 db = SQLAlchemy(app)
 
-# ---------------------------------------------------------------------------------- #
-# TODO: This section is only for testing purposes. Delete it.
-class Test(db.Model):
-    __tablename__ = 'test'
-    id = db.Column('id', db.Unicode(36), unique=True, primary_key=True)
+# --------------------------------------------------------------------------- #
 
-tests = Test.query.all()
+# TODO: Move the models somewhere else.
 
-for test in tests:
-    print(test.id)
+class ContactPeople(db.Model):
+    __tablename__ = 'contact_people'
+    person_id = db.Column(
+        'person_id', 
+        db.Integer,
+        unique=True,
+        primary_key=True,
+        nullable=False
+    )
+    full_name = db.Column(
+        'full_name',
+        db.String(255),
+        nullable=False
+    )
+    address = db.Column(
+        'address',
+        db.String(255),
+        nullable=False
+    )
+    postal_code = db.Column(
+        'postal_code',
+        db.String(100),
+        nullable=False
+    )
+    e_mail = db.Column(
+        'e_mail',
+        db.String(100),
+        nullable=False
+    )
+    phone = db.Column(
+        'phone',
+        db.String(100),
+        nullable=False
+    )
 
-# ---------------------------------------------------------------------------------- #
+class Application(db.Model):
+    __tablename__ = 'applications'
+    application_id = db.Column(
+        'application_id', 
+        db.Integer,
+        unique=True,
+        primary_key=True,
+        nullable=False
+    )
+    application_uuid = db.Column(
+        'application_uuid', 
+        db.Unicode(36),
+        unique=True,
+        nullable=False
+    )
+    application_type = db.Column(
+        'application_type',
+        db.String(50),
+        nullable=False
+    )
+    title = db.Column(
+        'title',
+        db.String(255),
+        nullable=False
+    )
+    relevant_interests = db.Column(
+        'relevant_interests',
+        db.Text,
+        nullable=False
+    )
+    self_government = db.Column(
+        'self_government',
+        db.String(500),
+        nullable=False
+    )
+    contact_person_id = db.Column(
+        'contact_person_id', 
+        db.Integer,
+        nullable=False
+    )
+    applicant_website = db.Column(
+        'applicant_website',
+        db.String(2083),
+        nullable=False
+    )
+    project_duration_in_months = db.Column(
+        'project_duration_in_months', 
+        db.SmallInteger,
+        nullable=False
+    )
+    detailed_description = db.Column(
+        'detailed_description',
+        db.Text,
+        nullable=False
+    )
+    innovation = db.Column(
+        'innovation',
+        db.Text,
+        nullable=False
+    )
+    project_activities = db.Column(
+        'project_activities',
+        db.Text,
+        nullable=False
+    )
+    expected_outcomes = db.Column(
+        'expected_outcomes',
+        db.Text,
+        nullable=False
+    )
+    gender_approach = db.Column(
+        'gender_approach',
+        db.Text,
+        nullable=False
+    )
+    project_budget = db.Column(
+        'project_budget',
+        db.Text,
+        nullable=False
+    )
 
-@app.route('/')
+# TODO: Add the `application_id` foreign key.
+
+class ProjectSummary(db.Model):
+    __tablename__ = 'project_summaries'
+    application_id = db.Column(
+        'application_id', 
+        db.Integer,
+        unique=True,
+        primary_key=True,
+        nullable=False
+    )
+    summary_id = db.Column(
+        'summary_id', 
+        db.Integer,
+        unique=True,
+        primary_key=True,
+        nullable=False
+    )
+    language = db.Column(
+        'language', 
+        db.Unicode(2),
+        nullable=False
+    )
+    summary = db.Column(
+        'summary', 
+        db.Text,
+        nullable=False
+    )
+
+# --------------------------------------------------------------------------- #
+
+@app.route('/', methods=['GET'])
 def index():
-    return 'Hello there!'
+    tests = Application.query.all()
+
+    for test in tests:
+        print(test.id)
+    return 'TEST ROUTE WORKS!!!'
+
+@app.route('/', methods=['POST'])
+def test():
+    new_uuid = str(uuid.uuid4())
+    data = request.get_json()
+    new_contact_person = ContactPeople(
+        full_name=data['contact_person']['full_name'],
+        address=data['contact_person']['address'],
+        postal_code=data['contact_person']['postal_code'],
+        e_mail=data['contact_person']['e_mail'],
+        phone=data['contact_person']['phone'],
+    )
+    new_application = Application(
+        application_uuid=new_uuid,
+        application_type=data['application_type'],
+        title=data['title'],
+        relevant_interests=data['relevant_interests'],
+        self_government=data['self_government'],
+        contact_person_id=data['contact_person_id'],
+        applicant_website=data['applicant_website'],
+        project_duration_in_months=data['project_duration_in_months'],
+        detailed_description=data['detailed_description'],
+        innovation=data['innovation'],
+        project_activities=data['project_activities'],
+        expected_outcomes=data['expected_outcomes'],
+        gender_approach=data['gender_approach'],
+        project_budget=data['project_budget']
+    )
+    db.session.add(new_contact_person)
+    db.session.add(new_application)
+    db.session.commit()
+
+    new_project_summary_en = ProjectSummary(
+        application_id=1, # TODO: Add the newly inserted application ID.
+        language='EN',
+        summary=data['project_summary_en']['summary']
+    )
+    new_project_summary_sr = ProjectSummary(
+        application_id=1, # TODO: Add the newly inserted application ID.
+        language='SR',
+        summary=data['project_summary_sr']['summary']
+    )
+    db.session.add(new_project_summary_en)
+    db.session.add(new_project_summary_sr)
+    db.session.commit()
+    
+    return jsonify({'uuid': new_uuid}) 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
